95e542f4519a1ca7c86aaea7723a0aaaef05e479
77leviann <77leviann@gmail.com>
2024-04-11 20:09:27 +0700

    fix:image

--- src/scripts/data/latestWorkData.js
+++ src/scripts/data/latestWorkData.js
@@ -3,7 +3,7 @@ export const latestWorkData = [
     id: 1,
     title: "SimpleBiz",
     description: "C523-PS036's Capstone Project MSIB Batch 5 Front-End and Back-End Dicoding Indonesia.",
-    imageURL: "../../public/simplebiz-project.PNG",
+    imageURL: "../../public/simplebiz-project.png",
     websiteURL: "https://simplebiz.netlify.app/",
   },
 ];
